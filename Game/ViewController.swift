f529c9d1620956c4c42421a6caf9445d0131a730
xxxThinking <973986153@qq.com>
2019-06-11 19:28:57 +0800

    Initial Commit

---
+++ Game/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Game
+//
+//  Created by XXXTHINKING on 2019/6/11.
+//  Copyright © 2019 XXXTHINKING. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
